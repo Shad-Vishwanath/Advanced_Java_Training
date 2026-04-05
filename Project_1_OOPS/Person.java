5b590117a8b7b82c8f781f0045e8f85758c03268
Shad_Vishwa <vishwanaths1006@gmail.com>
2026-04-05 18:33:41 +0530

    Add abstract Person class with attributes and methods

---
+++ Project_1_OOPS/Person.java
@@ -0,0 +1,31 @@
+abstract class Person {
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+
+    public Person(int id, String name, int age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public abstract void displayInfo();
+}
